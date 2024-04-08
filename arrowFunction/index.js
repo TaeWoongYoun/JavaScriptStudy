936249d2560950ba6319bcbfbb2bc9875ea38530
xodnd0507 <24457545yong@gmail.com>
2024-04-08 17:15:07 +0900

    test : Add => study update

---
+++ arrowFunction/index.js
@@ -0,0 +1,27 @@
+// var hamsu = (a) => {
+//     return a + 10
+// }
+// hamsu(5);
+
+
+// 코드가 한줄이면 중괄호 소괄호 생략 가능
+// var hamsu = a => a + 10;
+// hamsu(5);
+
+
+// var hamsu = (a,b) => {
+//     return a + 10 + b
+// }
+// console.log(hamsu(5, 7))
+
+// [1,2,3,4].forEach( a => console.log(a))
+
+// var object = {
+//     hamsu : () => {
+
+//     }
+// }
+
+// object.hamsu();
+
+// 에로우 펑션은 this와 사용하면 안됨.
